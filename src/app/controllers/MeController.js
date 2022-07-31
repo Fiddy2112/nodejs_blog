7b58d52e0cd2341a6367495c6c608d8428ba13de
Fiddy <85234199+Felixx2112@users.noreply.github.com>
2022-07-31 19:56:43 +0700

    npm mongoose-delete with method deleted count documents

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,13 +4,27 @@ const { mutipleMongooseToObject } = require("../../util/mongoose");
 class MeController {
   //[GET] /me/stored/courses
   storedCourses(req, res, next) {
-    Course.find({})
-      .then((course) => {
+    Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+      .then(([courses, deletedCount]) => {
         res.render("me/stored-courses", {
-          courses: mutipleMongooseToObject(course),
+          deletedCount,
+          courses: mutipleMongooseToObject(courses),
         });
       })
       .catch(next);
+
+    // Course.countDocumentsDeleted()
+    //   .then((deleteCount) => {
+    //     console.log(deleteCount);
+    //   })
+    //   .catch(() => {});
+    // Course.find({})
+    //   .then((course) => {
+    //     res.render("me/stored-courses", {
+    //       courses: mutipleMongooseToObject(course),
+    //     });
+    //   })
+    //   .catch(next);
   }
 
   //[GET] /me/trash/courses
